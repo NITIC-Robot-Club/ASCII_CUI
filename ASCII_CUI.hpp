dc5371fbc63edcbb0626f23eb19bbe577d166a73
kazu-321 <kzs321kzs@gmail.com>
2025-01-21 16:33:31 +0900

    [update] sample削除, public対応

--- ASCII_CUI.hpp
+++ ASCII_CUI.hpp
@@ -4,7 +4,6 @@
 #include <iostream>
 #include <string>
 #include <vector>
-#include <typeindex>
 #include <unordered_map>
 
 namespace ASCII_CUI {
@@ -113,86 +112,87 @@ class Layout;
 class Label {
 public:
     Label(const std::string& title, const std::string& text, Variable variable = vSet(nullptr))
-        : title_(title), text_(text), variable_(variable) {}
-    void setTitle(const std::string& title) { title_ = title; }
-    void setText(const std::string& text) { text_ = text; }
-    void setColor(Color color) { color_ = color; }
-    void setBGColor(Color bgcolor) { bgcolor_ = bgcolor; }
-    void setStyle(Style style) { style_ = style; }
-    void setSelectedColor(Color color) { selected_color_ = color; }
-    void setSelectedBGColor(Color bgcolor) { selected_bgcolor_ = bgcolor; }
-    void setSelectedStyle(Style style) { selected_style_ = style; }
-    void setNext(Layout* next) { next_ = next; }
-    template <typename T>
-    void setVariable(T* variable) {
-        variable_ = vSet(variable);
-    }
+        : title(title), text(text), variable(variable) {}
+
+    std::string title = "";
+    std::string text = "";
+    Layout* next = nullptr;
+    Color color = Color::Normal;
+    Color bgcolor = Color::Normal;
+    Style style = Style::Reset;
+    Color selected_color = Color::Normal;
+    Color selected_bgcolor = Color::Normal;
+    Style selected_style = Style::Underline;
+    Variable variable = vSet(nullptr);
+
+    
     void select() {
-        if(variable_.type == -1) {
+        if(variable.type == -1) {
             return;
         }
-        if(variable_.type == 5) {
-            *(bool*)variable_.address[variable_.type] = !*(bool*)variable_.address[variable_.type];
+        if(variable.type == 5) {
+            *(bool*)variable.address[variable.type] = !*(bool*)variable.address[variable.type];
         } else {
             std::cout << "\033[2J";
-            std::cout << "\033[2;1H\033[J" << text_ << std::endl;
-            std::cout << title_ << "の値を入力してください (" << get_typename(variable_.type) << ")" <<std::endl;
-            std::cout << "現在の値 : " << variable_ << std::endl;
+            std::cout << "\033[2;1H\033[J" << text << std::endl;
+            std::cout << title << "の値を入力してください (" << get_typename(variable.type) << ")" <<std::endl;
+            std::cout << "現在の値 : " << variable << std::endl;
             std::cout << ">>> ";
-            std::cin >> variable_;
+            std::cin >> variable;
             std::cout << "\033[2J";
         }
     }
 
     void print() {
-        std::cout << "\033[" << (int)style_ << ";" << (int)color_ + 30 << ";" 
-                  << (int)bgcolor_ + 40 << "m" << title_ << "\033[0m";
+        std::cout << "\033[" << (int)style << ";" << (int)color + 30 << ";" 
+                  << (int)bgcolor + 40 << "m" << title << "\033[0m";
     }
 
     void printSelected(int n) {
-        std::cout << "\033[" << (int)selected_style_ << ";" 
-                  << (int)selected_color_ + 30 << ";" 
-                  << (int)selected_bgcolor_ + 40 << "m" 
-                  << title_ << "\033[0m";
-        std::cout << "\033[" << n << ";1H\033[J" << text_;
-        if (variable_.type != -1) {
-            std::cout << " : " << variable_;
+        std::cout << "\033[" << (int)selected_style << ";" 
+                  << (int)selected_color + 30 << ";" 
+                  << (int)selected_bgcolor + 40 << "m" 
+                  << title << "\033[0m";
+        std::cout << "\033[" << n << ";1H\033[J" << text;
+        if (variable.type != -1) {
+            std::cout << " : " << variable;
         }
     }
-    Layout* next() { return next_; }
-
-private:
-    std::string title_;
-    std::string text_;
-    Variable variable_;
-    Layout* next_ = nullptr;
-    Color color_ = Color::Normal;
-    Color bgcolor_ = Color::Normal;
-    Style style_ = Style::Reset;
-    Color selected_color_ = Color::Normal;
-    Color selected_bgcolor_ = Color::Normal;
-    Style selected_style_ = Style::Underline;
 };
 
 
 class Layout {
 public:
     Layout(std::initializer_list<Label> labels) : labels_(labels) {}
-    Label* at(int index) { return &labels_[index]; }
-    int size() { return labels_.size(); }
+
+    Label& operator[](size_t index) {
+        return labels_.at(index);
+    }
+
+    size_t size() const {
+        return labels_.size();
+    }
+
+    Label* at(size_t index) {
+        if (index < labels_.size()) {
+            return &labels_.at(index);
+        }
+        return nullptr;
+    }
+
 private:
     std::vector<Label> labels_;
 };
 
+
 class UI {
 public:
     UI(Layout* layout) : current_layout_(layout), selected_index_(0) { std::cout << "\033[2J"; }
+
     void print() {
-        // move to 0,0
         std::cout << "\033[0;0H";
         for (int i = 0; i < current_layout_->size(); i++) {
             if (i == selected_index_) {
-                // move to i,0
                 std::cout << "\033[" << i+1 << ";1H"; 
                 std::cout << " > ";
                 current_layout_->at(i)->printSelected(current_layout_->size()+2);
@@ -216,7 +216,6 @@ public:
         std::cout << "\033[2J";
     }
 
-
     void up() {
         if (selected_index_ > 0) {
             selected_index_--;
@@ -230,7 +229,7 @@ public:
     }
 
     void enter() {
-        auto next = current_layout_->at(selected_index_)->next();
+        auto next = current_layout_->at(selected_index_)->next;
         if (next != nullptr) {
             current_layout_ = next;
             selected_index_ = 0;
@@ -243,7 +242,6 @@ public:
 private:
     Layout* current_layout_;
     int selected_index_;
-
 };
 
 } // namespace ASCII_CUI
--- README.md
+++ README.md
@@ -1,5 +1,52 @@
 # ascii_cui
 ASCIIコードのキー入力(矢印、スペース、エンター)でCUI(BIOS的な？)を動かす
 
-# How to use?
-1. include "ASCII_CUI.hpp"
+# サンプルコード
+```cpp
+#include "ASCII_CUI.hpp"
+
+bool EMG_RQ=0, OVA_EMG_EN=0, UVA_EMG_EN=0, OIA_EMG_EN=0;
+float V_LIMIT_HIGH=4.2;
+
+using namespace ASCII_CUI;
+Layout main_layout = {
+    {"駆動電源基板設定", "-> drive_power"},
+    {"制御電源基板設定", "-> control_power"},
+    {"ロボマス制御基板設定", "-> robomas"}
+};
+Layout drive_power_layout = {
+    {"戻る", "-> main"},
+    {"EX_EMG_TRG","自動非常停止設定"},
+    {"EMG_RQ","非常停止要求", vSet(&EMG_RQ)},
+    {"V_LIMIT_HIGH","セル当たりの最大電圧アラート", vSet(&V_LIMIT_HIGH)}
+};
+Layout EX_EMG_TRG_layout = {
+    {"戻る", "-> drive_power"},
+    {"OVA_EMG_EN", "過電圧アラート時", vSet(&OVA_EMG_EN)},
+    {"UVA_EMG_EN", "低電圧アラート時", vSet(&UVA_EMG_EN)},
+    {"OIA_EMG_EN", "過電流アラート時", vSet(&OIA_EMG_EN)}
+};
+
+
+UI ui(&main_layout);
+
+
+int main() {
+    main_layout.at(0)->setNext(&drive_power_layout);
+    drive_power_layout.at(0)->setNext(&main_layout);
+    drive_power_layout.at(1)->setNext(&EX_EMG_TRG_layout);
+    EX_EMG_TRG_layout.at(0)->setNext(&drive_power_layout);
+    while (true) {
+        ui.print();
+        char c = getchar();
+        if (c == 'w') {
+            ui.up();
+        } else if (c == 's') {
+            ui.down();
+        } else if (c == 'e') {
+            ui.enter();
+        }
+    }
+    return 0;
+}
+```
